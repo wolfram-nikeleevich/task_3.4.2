ad956b3dfb526ce3f7fd1f5739be4f6f70a8b765
Владимир Сизов <7580844@mail.ru>
2022-02-28 14:43:38 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
 
-    service.calculate(amount, registered);
-  }
+        long actual = service.calculate(amount, registered);
+        boolean pass = expected == actual;
+        if (pass) {
+            System.out.println("test passed");
+        } else {
+            System.out.println("test failed");
+        }
+    }
 }
 
